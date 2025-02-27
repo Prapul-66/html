aea0ecc3da2b519c6690e5f44e9a26320ccfb7a6
Prapul-66 <sprapul12@gmail.com>
2025-02-27 23:41:37 +0530

    commit

---
+++ html/index.js
@@ -0,0 +1,3 @@
+// This file is empty for now but could contain interactivity later!
+// Example: Add interactivity to categories or autoplay trailers on hover.
+console.log("Netflix Clone Loaded!");
